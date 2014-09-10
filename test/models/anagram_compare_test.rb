1677d9ca7fa2d5241afab05b779fe560c6fd600d
Rafael Escrich <rafaelescrich@secplus.com.br>
2014-09-10 16:38:41 -0300

    initiated tests

--- test/models/anagram_compare_test.rb
+++ test/models/anagram_compare_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class AnagramCompareTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  describe AnagramCompare do
+    
+    before :each do
+      @anagram_compare = AnagramCompare.new "original", "wannabe", :status
+    end
+
+    describe "#new" do
+      it "returns a new anagram object" do
+        @anagram_compare.should be_and_instance_of AnagramCompare
+      end
+	end
+  end
 end
