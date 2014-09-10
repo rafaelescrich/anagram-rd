410ef38cf741f7276ab66d9e6103806ac87da664
Rafael Escrich <rafaelescrich@secplus.com.br>
2014-09-10 18:01:44 -0300

    fixed return true in before_save

--- app/models/anagram_compare.rb
+++ app/models/anagram_compare.rb
@@ -3,10 +3,11 @@ class AnagramCompare < ActiveRecord::Base
 	before_create :status_true
 
 	def status_true
-  		self.status = self.clean(original) == self.clean(wannabe) ? true : false
-  		return true
+  		self.status = self.clean(original) == self.clean(wannabe)
+  		self.status != nil
   	end
-  	def clean(word)
-		word.gsub(/\s+/, "").mb_chars.downcase.to_s.chars.sort{ |a, b| a.casecmp(b) }.join
+  	
+  	def clean(string)
+		string.gsub(/\s+/, "").mb_chars.downcase.to_s.chars.sort{ |a, b| a.casecmp(b) }.join
 	end
 end
